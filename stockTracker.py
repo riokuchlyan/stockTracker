8c061e2e1e2dc410f6447100b7f223460c9d33a5
Rio Kuchlyan <riokuchlyan@gmail.com>
2024-11-05 10:52:10 +0000

    Add polygon API

--- stockTracker.py
+++ stockTracker.py
@@ -1,1 +1,18 @@
+from polygon import RESTClient
+
+client = RESTClient("y0pnIt8YeMJpLoSTQyFzTjU1vXPpsXlM")
+
+aggs = []
+for a in client.list_aggs(
+    "AAPL",
+    1,
+    "minute",
+    "2022-01-01",
+    "2023-02-03",
+    limit=50000,
+):
+    aggs.append(a)
+
+print(aggs)
+
 #use X and polygon API to give information on inputed stock and use X sentiment analysis to make predictions
